a5a25f0e41844086dff76e7940c5995e1a0a80df
Swype545 <13069@ecam.be>
2019-10-28 20:19:02 +0100

    translations

--- public/locale/fr.js
+++ public/locale/fr.js
@@ -1,8 +1,30 @@
 
 var fr = {
-	"about-us" : "about US FR",
 	"title.name" : "Librairie Alpha",
-	"title.quote" : "Citation spéciale",
-	
+	"title.quote" : "Tous les livres du monde entier",
+	"contact-us.title":"Contactez-nous",
+	"contact-us.address-title":"Adresse",
+	"contact-us.address-1":"Librairie Universitaire Alpha",
+	"contact-us.address-2":"Rue de Termonde, 140-142",
+	"contact-us.address-3":"BE - 1083 Bruxelles",
+	"contact-us.phone-title":"Téléphone",
+	"contact-us.phone":"02/468.36.31",
+	"contact-us.tva-title":"TVA",
+	"contact-us.tva":"BE 0893 555 882",
+	"contact-us.more-info-title":"Plus d'informations",
+	"contact-us.more-info":"Nous trouvons pour vous n'importe quel livre dans le monde.",
+	"about-us.title":"A propos de nous",
+	"about-us.content-1":"Depuis 1833, nous fournissons tous types de publications, quel que soit le pays d’origine ou la source (éditeur professionnel ou non-professionnel) à des sociétés et institutions.",
+	"about-us.content-2":"Nous nous chargeons de vous fournir en version papier ou en version PDF les publications suivantes:  livres, actes de congrès, thèses, cours, normes, standards,  documents officiels,  études,  livres épuisés, etc...  quelque soit la langue ou le pays d'origine.",
+	"warning-1":"Vente uniquement aux sociétés et aux institutions",
+	"warning-2":"PAS DE VENTE AUX PARTICULIERS !",
+	"warning-3":"!!! Grossiste !!! <br/> !!! Pas de vente aux particuliers !!!",
+	"form.company":"Société",
+	"form.last-name":"Nom de famille",
+	"form.email":"Email",
+	"form.phone":"Téléphone",
+	"form.subject":"Sujet",
+	"form.message":"Message",
+	"form.mandatory-fields":"* Champs obligatoires",
 }
 
